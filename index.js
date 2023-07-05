c12460ce93604aade06e03be1efeec0445fe9060
Suncerie Daye <sunceried@gmail.com>
2023-07-05 13:59:11 -0700

    Particle class started

--- index.js
+++ index.js
@@ -15,3 +15,44 @@ tagCardButtons.forEach((button) => {
 
     });
 });
+
+
+/* Particle background */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+
+ window.addEventListener('mousemove',
+    (event) => {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+ );
+
+ class Particle {
+    constructor(x, y, directionX, directionY, size, color){
+        this.x =x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#9d8189';
+        ctx.fill();
+    }
+ }
